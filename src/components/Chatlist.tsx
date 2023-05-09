e9d63b1dbc62bd91339a82e8ba9942713e4ade5b
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-09 17:15:21 +0530

    updated

--- src/components/Chatlist.tsx
+++ src/components/Chatlist.tsx
@@ -1,21 +1,29 @@
+import Image from "next/image";
 
 export default function Chatlist() {
   return (
     <>
-    <div className="bg-gray-100">
-        <ul className="mt-4">
-            <li className="flex items-center px-4 py-2 hover:bg-gray-200">
-            <img src="https://source.unsplash.com/random/32x32" alt="Profile" className="w-8 h-8 rounded-full mr-2" />
-            <span className="text-gray-800">John Doe</span>
-            </li>
-            <li className="flex items-center px-4 py-2 hover:bg-gray-200">
-            <img src="https://source.unsplash.com/random/32x32" alt="Profile" className="w-8 h-8 rounded-full mr-2" />
-            <span className="text-gray-800">Jane Smith</span>
-            </li>
-            {/* ... more chat contacts */}
-        </ul>
-    </div>
+      <div className="flex space-x-4 text-[#436475] items-center justify-between w-5/6 h-20 p-5 bg-black">
+        <div className="flex items-center space-x-2">
+          <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
+          <div className="flex flex-col">
+            <span className="text-sm font-semibold">Shivrajjj</span>
+            <span className="text-xs font-light">Hey there....</span>
+          </div>
+        </div>
+        <span className="font-semibold text-xs">2 minutes</span>
+      </div>
+      {/* seperated */}
+      <div className="flex space-x-4 text-[#436475] items-center justify-between w-5/6 h-20 p-5 bg-red-600">
+        <div className="flex items-center space-x-2">
+          <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
+          <div className="flex flex-col">
+            <span className="text-sm font-semibold">Shivrajjj</span>
+            <span className="text-xs font-light">Hey there....</span>
+          </div>
+        </div>
+        <span className="font-semibold text-xs">Yesterday</span>
+      </div>
     </>
   );
 };
-
