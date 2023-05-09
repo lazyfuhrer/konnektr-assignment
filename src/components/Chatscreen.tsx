d466636a069e4f5228e0d6cf15f92b153534de5f
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-09 21:47:55 +0530

    updated

--- src/components/Chatscreen.tsx
+++ src/components/Chatscreen.tsx
@@ -1,18 +1,16 @@
+import moment from "moment";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function ChatScreen (){
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
-
   const recipient = { name: "Shivrajjj", profileImage: "/images/profileBig.svg" };
-
   const handleSendMessage = () => {
     if (message.trim().length === 0) return;
     setMessages([...messages, message.trim()]);
     setMessage("");
   };
-
   return (
     <div className="flex flex-col h-screen bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url(images/chatbg.svg)" }}>
       {/* Header section */}
@@ -26,11 +24,20 @@ export default function ChatScreen (){
           <Image src={"/images/video.svg"} width={20} height={20} alt="video"/>
         </div>
       </div>
+      {/* Date section */}
+      <div className="flex items-center justify-center mt-4">
+        <hr className="border-t border-gray-400 flex-grow mx-3" />
+        <p className="text-sm font-medium text-[#436475]">{moment().format("MMM D, YYYY")}</p>
+        <hr className="border-t border-gray-400 flex-grow mx-3" />
+      </div>
       {/* Chat area */}
       <div className="flex-1 flex flex-col overflow-y-auto">
         {messages.map((msg, idx) => (
-          <div key={idx} className={`flex items-center my-2 ${ idx % 2 === 0 ? "justify-start" : "justify-end"}`}>
-            <div className={`px-4 m-5 py-2 rounded-lg ${ idx % 2 === 0 ? "bg-[#FE06C7]" : "bg-white text-black" }`}>{msg}</div>
+          <div key={idx} className={`flex items-start my-2 ${ idx % 2 === 0 ? "ml-5 justify-start" : "justify-end mr-5"}`}>
+            <div className={`max-w-xs px-4 py-2 rounded-lg ${ idx % 2 === 0 ? "bg-[#FE06C7] text-white" : "bg-white text-black " }`}>
+              <p className="text-base">{msg}</p>
+              <p className="text-xs text-gray-400 text-right mt-1">{moment().format("h:mm A")}</p>
+            </div>
           </div>
         ))}
       </div>
