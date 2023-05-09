ffd6559c1956464db74b33a69e4f34a29c2d6978
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-09 18:07:01 +0530

    updated

--- src/components/Chatlist.tsx
+++ src/components/Chatlist.tsx
@@ -1,7 +1,32 @@
 import Image from "next/image";
-import { FaSearch } from "react-icons/fa";
+import { FaCircle, FaSearch } from "react-icons/fa";
 
 export default function Chatlist() {
+  const timeStamp = [
+    { isOnline: true,
+      time: "2 minutes",
+    },
+    {
+      isOnline: false,
+      time: "1 Hour",
+    },
+    {
+      isOnline: true,
+      time: "Yesterday",
+    },
+    {
+      isOnline: false,
+      time: "Just Now",
+    },
+    {
+      isOnline: true,
+      time: "Just Now",
+    },
+    {
+      isOnline: false,
+      time: "Yesterday",
+    },
+  ];
   return (
     <div className="w-5/6 mx-auto text-[#436475]">
       <div className="flex items-center justify-between w-full h-20 px-5 rounded-3xl">
@@ -15,27 +40,23 @@ export default function Chatlist() {
         </div>
       </div>
       <h1 className="text-left font-bold text-xl mb-5">Chats</h1>
-      <div className="flex space-x-4 items-center justify-between h-20 p-5 bg-black rounded-md">
-        <div className="flex items-center space-x-2">
-          <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
-          <div className="flex flex-col">
-            <span className="text-sm font-semibold">Shivrajjj</span>
-            <span className="text-xs font-light">Hey there....</span>
+      {timeStamp.map(({isOnline, time}, index) => (
+        <div key={index} className="flex space-x-4 text-[#436475] items-center justify-between h-20 p-5 rounded-md">
+          <div className="flex items-center space-x-2">
+            <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
+            {isOnline && (
+              <div className="absolute bottom-0 right-0">
+                <FaCircle className="text-green-500 text-xs" />
+              </div>
+            )}
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold">Shivrajjj</span>
+              <span className="text-xs font-light">Hey there....</span>
+            </div>
           </div>
+          <span className="font-semibold text-xs">{time}</span>
         </div>
-        <span className="font-semibold text-xs">2 minutes</span>
-      </div>
-      {/* separated */}
-      <div className="flex space-x-4 text-[#436475] items-center justify-between h-20 p-5 bg-red-600 rounded-md">
-        <div className="flex items-center space-x-2">
-          <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
-          <div className="flex flex-col">
-            <span className="text-sm font-semibold">Shivrajjj</span>
-            <span className="text-xs font-light">Hey there....</span>
-          </div>
-        </div>
-        <span className="font-semibold text-xs">Yesterday</span>
-      </div>
+      ))}
     </div>
   );
 };
