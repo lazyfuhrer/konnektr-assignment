080a4a7f05ba1a3a72ff606dc3d436012f5f225b
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-09 17:42:36 +0530

    updated

--- src/components/Chatlist.tsx
+++ src/components/Chatlist.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { FaSearch } from "react-icons/fa";
 
 export default function Chatlist() {
   return (
-    <>
-      <div className="flex space-x-4 text-[#436475] items-center justify-between w-5/6 h-20 p-5 bg-black">
+    <div className="w-5/6 mx-auto text-[#436475]">
+      <div className="flex items-center justify-between w-full h-20 px-5 rounded-3xl">
+        <div className="w-310 h-46 bg-gray-100 rounded-md flex items-center px-4 bg-red">
+          <FaSearch className="text-gray-400 mr-3" />
+          <input
+            type="text"
+            className="w-full h-full font-bold bg-transparent focus:outline-none"
+            placeholder="Search"
+          />
+        </div>
+      </div>
+      <h1 className="text-left font-bold text-xl mb-5">Chats</h1>
+      <div className="flex space-x-4 items-center justify-between h-20 p-5 bg-black rounded-md">
         <div className="flex items-center space-x-2">
           <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
           <div className="flex flex-col">
@@ -13,8 +25,8 @@ export default function Chatlist() {
         </div>
         <span className="font-semibold text-xs">2 minutes</span>
       </div>
-      {/* seperated */}
-      <div className="flex space-x-4 text-[#436475] items-center justify-between w-5/6 h-20 p-5 bg-red-600">
+      {/* separated */}
+      <div className="flex space-x-4 text-[#436475] items-center justify-between h-20 p-5 bg-red-600 rounded-md">
         <div className="flex items-center space-x-2">
           <Image className='rounded-full' src={"/images/profile.svg"} width={50} height={50} alt="profile"/>
           <div className="flex flex-col">
@@ -24,6 +36,6 @@ export default function Chatlist() {
         </div>
         <span className="font-semibold text-xs">Yesterday</span>
       </div>
-    </>
+    </div>
   );
 };
