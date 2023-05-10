1c82a8f6b9f7699aa5e307425da364de816f3384
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-10 20:05:08 +0530

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,14 @@ import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
   return (
-    <div className="flex flex-row w-screen h-screen">
-      <div className="w-1/4 bg-gray-100">
+    <div className="flex flex-col md:flex-row w-screen h-screen">
+      <div className="w-full md:w-1/4 bg-gray-100 md:block hidden">
         <Sidebar/>
       </div>
-      <div className="w-1/2 bg-gray-200">
+      <div className="w-full md:w-1/2 bg-gray-200">
         <Chatscreen/>
       </div>
-      <div className="w-1/4 bg-gray-100">
+      <div className="w-full md:w-1/4 bg-gray-100 md:block hidden">
         <Chatlist/>
       </div>
     </div>
